283e72e95aa0228a5384a9e47aa31393771511dd
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-02-04 12:57:06 +0100

    add active account border

--- notes.rb
+++ notes.rb
@@ -13,7 +13,7 @@ end
 
 def client.get_all_tweets(user)
   collect_with_max_id do |max_id|
-    options = {count: 1, tweet_mode: "extended", include_rts: false}
+    options = {count: 200, tweet_mode: "extended", include_rts: false}
     options[:max_id] = max_id unless max_id.nil?
     user_timeline(user, options)
   end
