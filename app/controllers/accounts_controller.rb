8d240be4689419c39969e80216a07593fc9c5bf9
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-02-04 16:20:52 +0100

    set up pagination params

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -6,6 +6,6 @@ class AccountsController < ApplicationController
   def show
     @accounts = Account.all
     @account = Account.find(params[:id])
-    @tweets = @account.tweets.page(1)
+    @tweets = @account.tweets.page params[:page]
   end
 end
