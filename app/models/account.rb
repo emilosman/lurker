0cb197161fdcbcdc424d80b4229fa05d78d1489c
Emil Osmanbegovic <emilosmanbegovic@gmail.com>
2020-06-20 20:02:35 +0200

    fix media array

--- app/models/account.rb
+++ app/models/account.rb
@@ -18,19 +18,13 @@ class Account
         tweet_id: tw.id
       )
 
-      media_urls = []
-
-      if tw.media.present?
-        media_urls << tw.media.map{|m| m.attrs[:media_url_https]}
-      end
-
       tweet.update_attributes(
         favorite_count: tw.favorite_count,
         retweet_count: tw.retweet_count,
         full_text: tw.attrs[:full_text],
         url: tw.url.to_s,
         tweeted_at: tw.created_at,
-        media: media_urls
+        media: tw.media.map{|m| m.attrs[:media_url_https]} if tw.media.present?
       )
     end
   end
